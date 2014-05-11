8060b9520ad801c6ca09e67d4f4c595a6398c784
Kouki Saito <dan.addr.skd@gmail.com>
2014-05-11 21:14:42 +0900

    Add tips for editing secrets.py

--- secrets.py
+++ secrets.py
@@ -2,6 +2,8 @@
 
 cookie_secret="TODO:REPLACE FOR YOUR COOKIE SECRET"
 
-
-
+# The below two command is very usuful!
+# You can use these command when change this file and want to stage it.
+# git update-index --no-skip-worktree secrets.py
+# git update-index --skip-worktree secrets.py
 
